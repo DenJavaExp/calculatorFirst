2e2666b745c2c77b8370b8bf06575d9439d1afd0
DenJavaExp <marmyshev.denis@mail.ru>
2022-05-12 17:33:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.calc;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Double operand = null;
+    String lastOperation = "=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultField = findViewById(R.id.resultField);
+        numberField = findViewById(R.id.numberField);
+        operationField = findViewById(R.id.operationField);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+
+    public void onNumberClick(View view){
+        Button button =(Button) view;
+        numberField.append((button.getText()));
+
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+
+
+    }
+}
